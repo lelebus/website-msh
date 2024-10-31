79b8eb2536694bb6536bae18038d5af8d2b587f3
Gabriele De Candido <gabrieledecandido@flashbeing.com>
2024-10-31 21:20:13 +0100

    feat: spa page

---
+++ components/sections/FeaturesList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="bg-white">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div
+                class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+                <dl
+                    class="col-span-4 grid grid-cols-1 gap-x-8 gap-y-10 text-base/7 text-black/60 sm:grid-cols-2 lg:grid-cols-4 lg:gap-y-16">
+                    <div v-for="feature in features" :key="feature.name" class="relative pl-9">
+                        <dt class="font-semibold text-black/90">
+                            <CheckIcon class="absolute left-0 top-1 h-5 w-5 text-msh" aria-hidden="true" />
+                            {{ feature.name }}
+                        </dt>
+                        <dd class="mt-2">{{ feature.description }}</dd>
+                    </div>
+                </dl>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { CheckIcon } from '@heroicons/vue/20/solid'
+</script>
+
+<script>
+export default {
+    props: {
+        features: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
---
+++ pages/spa-and-pool.vue
@@ -0,0 +1,123 @@
+<template>
+  <NuxtContent>
+    <Hero :titleLabel="hero.titleLabel" :title="hero.title" :subtitle="hero.subtitle"
+      :primaryButtonLabel="hero.primaryButtonLabel" :primaryButtonLink="hero.primaryButtonLink"
+      :secondaryButtonLabel="hero.secondaryButtonLabel" :secondaryButtonLink="hero.secondaryButtonLink"
+      :videoSrc="hero.videoSrc" @scrollToNextSection="scrollToStart" />
+
+    <div id="start" class="content-ct">
+            <div class="py-24 sm:py-32">
+        <SectionHeader :title="features.title" :subtitle="features.subtitle" />
+        <FeaturesList  :features="features.items" />
+      </div>
+
+      <ContentLxPicture :title="pool.title" :text="pool.text" :imgSrc="pool.imgSrc" :imgAlt="pool.title"
+        class="pb-24 sm:pb-32" />
+
+      <ContentRxPicture :title="spa.title" :text="spa.text" :imgSrc="spa.imgSrc" :imgAlt="spa.title"
+        class="pb-24 sm:pb-32" />
+
+      <LatestOffers class="mx-auto max-w-7xl sm:px-6 lg:px-8" />
+    </div>
+  </NuxtContent>
+</template>
+
+<script setup>
+useHead({
+  title: "SPA & Pool"
+}, {
+  tagPriority: 'critical'
+})
+
+useSeoMeta({
+  title: "SPA & Pool"
+}, {
+  tagPriority: 'critical'
+})
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Monaco Sport Hotel',
+      mission: this.$t('msh.mission'),
+      logoSrc: '/logo.svg',
+      email: 'info@monacosporthotel.com',
+
+      // hero
+      hero: {
+        videoSrc: `/videos/spa-and-pool.mp4`,
+        title: this.$t('pages.spaAndPool.heroTitle'),
+        subtitle: this.$t('pages.spaAndPool.heroSubtitle'),
+      },
+
+      // sections
+      pool: {
+        title: this.$t('pages.spaAndPool.poolTitle'),
+        text: this.$t('pages.spaAndPool.poolText'),
+        imgSrc: '/imgs/comelico-estate.jpg',
+      },
+      spa: {
+        title: this.$t('pages.spaAndPool.spaTitle'),
+        text: this.$t('pages.spaAndPool.spaText'),
+        imgSrc: '/imgs/comelico-estate.jpg',
+      },
+      features: {
+        title: this.$t('pages.spaAndPool.featuresTitle'),
+        subtitle: this.$t('pages.spaAndPool.featuresSubtitle'),
+        items: [
+          {
+            name: this.$t('pages.spaAndPool.saunaTitle'),
+            description: this.$t('pages.spaAndPool.saunaText'),
+          },
+          {
+            name: this.$t('pages.spaAndPool.bagnoTurcoTitle'),
+            description: this.$t('pages.spaAndPool.bagnoTurcoText'),
+          },
+          {
+            name: this.$t('pages.spaAndPool.bagnoMediterraneoTitle'),
+            description: this.$t('pages.spaAndPool.bagnoMediterraneoText'),
+          },
+          {
+            name: this.$t('pages.spaAndPool.docciaTropicaleTitle'),
+            description: this.$t('pages.spaAndPool.docciaTropicaleText'),
+          },
+          {
+            name: this.$t('pages.spaAndPool.secchiataSiberianaTitle'),
+            description: this.$t('pages.spaAndPool.secchiataSiberianaText'),
+          },
+          {
+            name: this.$t('pages.spaAndPool.cascataGhiaccioTitle'),
+            description: this.$t('pages.spaAndPool.cascataGhiaccioText'),
+          },
+          {
+            name: this.$t('pages.spaAndPool.areaRelaxTitle'),
+            description: this.$t('pages.spaAndPool.areaRelaxText'),
+          },
+          {
+            name: this.$t('pages.spaAndPool.piscinaTitle'),
+            description: this.$t('pages.spaAndPool.piscinaText'),
+          },
+        ]
+      },
+
+      offers: {
+        title: this.$t('pages.offers.title'),
+      },
+    }
+  },
+
+  methods: {
+    scrollToStart() {
+      this.$router.push({ hash: '#start' })
+    }
+  }
+}
+</script>
+
+<style>
+html {
+  scroll-behavior: smooth;
+}
+</style>
